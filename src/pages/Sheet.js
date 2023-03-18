ff2389eab96614144da3aceb49c79461e5578fda
Camara Mohamed Salim <m.salimcamara@gmail.com>
2023-03-18 18:29:04 +0100

    feat(sheet): info container progress

--- src/pages/Sheet.js
+++ src/pages/Sheet.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Caroussel from "../components/Caroussel";
+import Data from "./../assets/data/logements.json";
+import Banner from "./../assets/images/banner.png";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import "./../styles/pages/Sheet.css";
@@ -8,6 +10,11 @@ import "./../styles/pages/Sheet.css";
 const Sheet = () => {
   const location = useLocation();
   const data = location;
+  const tmpData = Data[0];
+
+  useEffect(() => {
+    console.log("l15 ", tmpData);
+  }, []);
 
   return (
     <div>
@@ -15,6 +22,26 @@ const Sheet = () => {
         <Header />
         <div className="sheet--caroussel">
           <Caroussel />
+        </div>
+        <div className="sheet__info">
+          <div className="sheet__info__left">
+            <h1 className="sheet__info__left--title">{tmpData.title}</h1>
+            <h3 className="sheet__info__left--location">{tmpData.location}</h3>
+            <div className="tag">
+              <p className="tag--text">tag</p>
+            </div>
+          </div>
+          <div className="sheet__info__right">
+            <div className="sheet__info__right__top">
+              <p className="sheet__info__right__top--host">
+                {tmpData.host.name.split(" ")[0]}
+                <br />
+                {tmpData.host.name.split(" ")[1]}
+              </p>
+              <img src={Banner} className="sheet__info__right__top--avatar" />
+            </div>
+            <div className="sheet__info__right__bottom"></div>
+          </div>
         </div>
       </div>
       {/* <Footer /> */}
