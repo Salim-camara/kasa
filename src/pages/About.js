6fbc38161b1ab8339b142938d757f566fda329c6
Salim Camara <m.salimcamara@gmail.com>
2023-04-06 16:36:59 +0200

    feat(about): OK

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,13 +3,24 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Banner from "./../assets/images/banner2.png";
 import "./../styles/pages/About.css";
+import data from "./../assets/data/about.json";
+import Collapse from "../components/Collapse";
 
 const About = () => {
   return (
-    <div>
+    <div style={{ flex: 1 }}>
       <div className="about">
         <Header />
         <img src={Banner} className="about--img" />
+        <div className="about__collapse--container">
+          {data.map((el) => {
+            return (
+              <div className="about__collapse">
+                <Collapse title={el.title} description={el.description} />
+              </div>
+            );
+          })}
+        </div>
       </div>
       {/* <Footer /> */}
     </div>
